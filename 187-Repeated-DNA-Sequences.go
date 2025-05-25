2d092613d92d23b81b9c2f2a877ce1b4b9f23a75
andrehds <andrehds@gmail.com>
2025-05-25 20:58:09 -0300

    187.

---
+++ 187-Repeated-DNA-Sequences.go
@@ -0,0 +1,56 @@
+package main
+
+func findRepeatedDnaSequences(s string) []string {
+	if len(s) < 11 {
+		return []string{}
+	}
+
+	var a int
+
+	m := make(map[int]int, len(s)-9)
+
+    // fill the window
+	for i := 0; i <= 9; i++ {
+		a = (a << 2) | toBits(s[i])
+	}
+	m[a]++
+
+    // slide
+	for i := 10; i < len(s); i++ {
+		a = (a << 2 & 1048575) | toBits(s[i])
+		m[a]++
+	}
+
+    // we can trade some time to count elements of m with 
+    // values > 1 to avoid allocating extra space
+	res := make([]string, 0, len(m))
+	for k, v := range m {
+		if v > 1 {
+			res = append(res, toString(k))
+		}
+	}
+
+	return res
+}
+
+func toBits(b byte) int {
+	return int(b & 6 >> 1)
+}
+
+func toString(x int) string {
+	res := make([]byte, 0, 10)
+	for i := 9; i >= 0; i-- {
+		v := 3 << (i * 2) & x >> (i * 2)
+		switch v {
+		case 0:
+			res = append(res, 65)
+		case 1:
+			res = append(res, 67)
+		case 2:
+			res = append(res, 84)
+		case 3:
+			res = append(res, 71)
+		}
+	}
+	return string(res)
+}
